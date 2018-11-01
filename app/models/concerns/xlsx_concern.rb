eb692d73c36a78cbea60275974effb82f70916db
施永坚 <syj_188888888@163.com>
2018-11-01 08:57:19 +0800

    继续优化

--- app/models/concerns/xlsx_concern.rb
+++ app/models/concerns/xlsx_concern.rb
@@ -7,20 +7,22 @@ module XlsxConcern
   end
 
   module ClassMethods
+
     def export_xlsx(name: 'table')
       path = Rails.root.join('tmp').to_path + "/#{name}.xlsx"
-      generate(path, self.all)
+      generate(path)
       path
     end
 
-    def generate(path, data)
+    # 导出的数据
+    def generate(path)
       xlsx_file = WriteXLSX.new(path)
       xlsx_sheet = xlsx_file.add_worksheet
       attribute_names = self.attribute_names
       attribute_names.each_with_index do |name, index|
         xlsx_sheet.write(0, index, self.human_attribute_name(name))
       end
-      data.each_with_index do |user, index|
+      self.all.each_with_index do |user, index|
         attribute_names.each_with_index do |name, name_index|
           xlsx_sheet.write(index + 1, name_index, user[name])
         end
